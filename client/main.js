0f36f8592611e1f9dcd66046363fe4438ace7cea
HerbiShtini <herb_sh@hotmail.com>
2016-06-04 15:54:05 +0200

    Some changes

--- client/main.js
+++ client/main.js
@@ -1,58 +1,40 @@
+// import { Meteor } form 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Mongo } from 'meteor/mongo';
 
 NoteItems = new Mongo.Collection('noteitems');
 
-
-if( Meteor.isClient ){
-  Meteor.subscribe('notes');
-}
-
 /**
 * Template onCreated
 */
 Template.notes.helpers({
   notes() {
-      return NoteItems.find();
-  }
+      return NoteItems.find({});
+  },
 });
 
 /**
 * Events
 */
+Template.notes.onCreated(function(){
+    Meteor.subscribe('notes');
+});
+
 Template.notes.events({
   'submit #noteForm'(event, instance){
 
     const newNote = event.target.noteInputNew.value;
 
-    if (Meteor.isServer) {
-       Meteor.call('addNote', newNote);
-    }
+    Meteor.call('notes.insert', newNote);
 
     event.target.noteInputNew.value = "";
     event.preventDefault();
 
+  },
+  'click .remove-note' : function(){
+
+     Meteor.call('notes.remove', this )
+
   }
 });
-
-
-/****************************
-*  Server Code *
-*******************************/
-if( Meteor.isServer ){
-
-  Meteor.publish('notes', function() {
-      return NoteItems.find();
-  })
-
-  Meteor.methods({
-     addNote : function( newNote ){
-       NoteItems.insert({
-             'label' : newNote,
-             'time' : Date.now(),
-             'userId' : Meteor.userId()
-       })
-     }
-  });
-}
--- server/main.js
+++ server/main.js
@@ -1,5 +1,33 @@
-import { Meteor } from 'meteor/meteor';
+// import { Meteor } from 'meteor/meteor';
+
+NoteItems = new Mongo.Collection('noteitems');
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Meteor.publish('notes', function() {
+    return NoteItems.find({});
+})
+
+Meteor.methods({
+  // 'notes.insert'
+    'notes.insert' : function( newNote ){
+
+     console.log( Meteor.userId() );
+     // Make sure the user is logged in before inserting a task
+     NoteItems.insert({
+           'label' : newNote,
+           'time' : Date.now(),
+           'userId' : Meteor.userId()
+     });
+
+   },
+   'notes.update' : function(){
+
+   },
+   'notes.remove' : function( item ){
+     console.log("remove")
+      NoteItems.remove( item._id )
+   }
 });
